1dc1ce948d9e56ba36971061812bb434cb7c3b1e
Bruno Belino <164117761+devbelino@users.noreply.github.com>
2026-01-29 21:53:09 -0300

    Create 04_consultas_exemplo.sql

---
+++ sql/04_consultas_exemplo.sql
@@ -0,0 +1,7 @@
+CALL realizar_pagamento(1, '02/2026');
+
+SELECT * FROM pagamentos WHERE mes_referencia = '02/2026';
+
+SELECT id, titulo FROM livros ORDER BY id LIMIT 10;
+
+SELECT * FROM usuarios;
